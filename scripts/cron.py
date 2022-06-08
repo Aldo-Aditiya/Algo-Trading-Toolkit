acd5def4a61143e7dfc357609c1be75ed9e21cd7
Aldo-Aditiya <aldoan100@gmail.com>
2022-06-09 00:09:01 +0800

    Added script to run other python scripts, for use with cron

---
+++ scripts/cron.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import argparse
+import json
+import importlib
+from datetime import datetime, timedelta
+import pickle as pkl
+
+def read_config(config_filepath):
+    '''
+    Read JSON config file as dictionary
+    '''
+    with open(config_filepath) as f:
+        config_dict = json.load(f)
+    return config_dict
+
+# Read Config
+cron_config_filepath = "cron_config.json"
+config_dict = read_config(cron_config_filepath)
+
+data_pull_file_dir = config_dict["data_pull_file_dir"]
+data_pull_run = config_dict["data_pull_run"]
+
+paper_trading_file_dir = config_dict["paper_trading_file_dir"]
+paper_trading_config_dir = config_dict["paper_trading_config_dir"]
+paper_trading_config_run = config_dict["paper_trading_config_run"]
+
+# Read Cron History
+cron_history = "cron_history.pkl"
+
+# Run Data Pull
+print("\n--Data Pull")
+for runfile in data_pull_run:
+    print("Run Data Pull On: " + runfile)
+    data_pull_filepath = data_pull_file_dir + runfile
+    os.system("python " + data_pull_filepath)
+
+print("\n--Paper Trading")
+# Run Paper Trading
+for confile in paper_trading_config_run:
+    print("Run Paper Trading On: " + confile)
+    paper_trading_confpath = paper_trading_config_dir + confile
+    paper_trading_conf = read_config(paper_trading_confpath)
+    paper_trading_filepath = paper_trading_file_dir + paper_trading_conf['run_params']['strat_run_filename']
+    
+    os.system("python " + 
+              paper_trading_filepath + 
+              " --config_filepath=" + paper_trading_confpath + 
+              " --active=True")
