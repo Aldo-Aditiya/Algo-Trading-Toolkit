131523606df693b8c57d645452445fe41fb80bd7
Aldo-Aditiya <aldoan100@gmail.com>
2022-06-13 20:54:25 +0800

    Added script to automatically commit pulled data and ran strategies

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 python cron.py
+git add /workspace/202205_idx-trading/_benchmarks/paper_trading/
+git add /workspace/202205_idx-trading/_data/
+DATE 
+git commit -m "Cron for Paper Trading and Data - $DATE"
