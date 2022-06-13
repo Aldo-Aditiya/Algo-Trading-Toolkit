281b31bd5188cd1ef59e0e4516f89608367a0257
Aldo-Aditiya <aldoan100@gmail.com>
2022-06-13 20:54:49 +0800

    Added script to automatically commit data and benchmarked results

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -2,5 +2,5 @@
 python cron.py
 git add /workspace/202205_idx-trading/_benchmarks/paper_trading/
 git add /workspace/202205_idx-trading/_data/
-DATE 
-git commit -m "Cron for Paper Trading and Data - $DATE"
+git commit -m "Cron for Paper Trading and Data - $(date '+%Y-%m-%d')"
+git push origin master
