b68b3e474ede1bfb7fd1fbc55bf44976741a7138
Aldo-Aditiya <aldoan100@gmail.com>
2022-06-16 22:30:43 +0800

    Updated Cron shell script

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python cron.py
+/usr/local/bin/python /workspace/202205_idx-trading/scripts/cron.py
 git add /workspace/202205_idx-trading/_benchmarks/paper_trading/
 git add /workspace/202205_idx-trading/_data/
 git commit -m "Cron for Paper Trading and Data - $(date '+%Y-%m-%d')"
