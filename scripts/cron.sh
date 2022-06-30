036859a56399af0733b0c680420630c1cdf068b5
Aldo-Aditiya <aldoan100@gmail.com>
2022-06-30 21:25:14 +0700

    Updated Cron

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -2,5 +2,7 @@
 /usr/local/bin/python /workspace/202205_idx-trading/scripts/cron.py
 git add /workspace/202205_idx-trading/_benchmarks/paper_trading/
 git add /workspace/202205_idx-trading/_data/
+git add /workspace/202205_idx-trading/_metadata/data.csv
+git add /workspace/202205_idx-trading/_metadata/paper_trade.csv
 git commit -m "Cron for Paper Trading and Data - $(date '+%Y-%m-%d')"
 git push origin master
